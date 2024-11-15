bc5bf226315b100feff096d78832a14ab86e4ca1
theindigod <indigod@quadprep.org>
2024-11-15 11:15:03 -0500

    Add files via upload

---
+++ encoder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class encoder {
+	public static void main(String[]args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your message:");
+		String message = scanner.nextLine();
+		System.out.println("i");
+		int Key = scanner.nextInt();
+		String encode = " ";
+		for
+		
+		
+		
+		
+	}
+}
